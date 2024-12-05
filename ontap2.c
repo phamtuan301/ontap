e8a6211699aba8aca8fa6d2f63a2ee37135a2402
phamtuan301 <tuan13nt@gmail.com>
2024-12-05 11:45:41 +0700

    Update ontap2.c

--- ontap2.c
+++ ontap2.c
@@ -103,7 +103,7 @@ int main() {
                 // Tim kiem mot phan tu va in vi tri phan tu do tren mang
                 printf("Nhap gia tri phan tu can tim: ");
                 scanf("%d", &x);
-                found = 0;
+                found = -1;
                 for (int i = 0; i < n; i++) {
                     for (int j = 0; j < m; j++) {
                         if (arr[i][j] == x) {
